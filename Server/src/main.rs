b479ac63135b44cc17a1f372a689b3b0b73e286a
RealDoomGaming <m710hau@gmail.com>
2026-04-11 08:49:40 +0000

    Made the initial structure of the server

--- Server/src/main.rs
+++ Server/src/main.rs
@@ -1,3 +1,42 @@
+use std::{
+    // BufReader and prelude -> traits and types which let us read and write to stream
+    io::{BufReader, prelude::*},
+    net::{TcpListener, TcpStream},
+    error::Error,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        
+        match handle_connection(stream) {
+            Ok(resp) => {
+            }
+            Err(e) => {
+            }
+        }
+    }    
+}
+
+fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    // stream is mutable -> we can change it
+    let buff_reader = BufReader::new(&stream);
+    let request_line = buff_reader
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap(); 
+    
+    let status_line = "HTTP/1.1 200 OK";
+    let content = "Hellos :D";
+    let con_length = content.len();
+
+    let response = 
+        format!("{status_line}\r\nContent-Length: {con_length}\r\n\r\n{content}");
+    
+    stream.write_all(response.as_bytes())?;
+
+    Ok(())
 }
