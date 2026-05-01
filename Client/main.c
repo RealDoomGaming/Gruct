22f0bec68c8e27eb3dbb9b827a567901de22f0f1
RealDoomGaming <m710hau@gmail.com>
2026-05-01 19:36:13 +0200

    Started working on the client cli

---
+++ Client/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  // argc is the amount of arguments
+  // argv is the array of arguments
+  // argv[0] is the programm name
+
+  char *command = argv[1];
+
+  // check if command is pull, push, etc.
+
+  return 0;
+}
