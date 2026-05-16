d2bd3ad7d86cb649da3004b187b5d63d1f1c3c05
RealDoomGaming <m710hau@gmail.com>
2026-05-16 09:07:45 +0200

    Started working on the curl stuff for the client

--- Client/main.c
+++ Client/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <string.h>
+
+// for the cur
+#include <curl/curl.h>
 
 int main(int argc, char *argv[]) {
+  // once per programm we have to init the curl
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+
   // argc is the amount of arguments
   // argv is the array of arguments
   // argv[0] is the programm name
@@ -8,6 +15,15 @@ int main(int argc, char *argv[]) {
   char *command = argv[1];
 
   // check if command is pull, push, etc.
+  if (strcmp(command, "pl")) {
+    // for pulling files, repos and also the git keys
+
+  } else if (strcmp(command, "pshfl")) {
+    // for pushing a file
+
+  } else if (strcmp(command, "mkrp")) {
+    // for making a repo
+  }
 
   return 0;
 }
